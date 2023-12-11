bdccb61b1ed1371daef267f42b88e184793a8513
happysubin <asb0711@gmail.com>
2023-12-11 22:56:35 +0900

    elasticsearch autoconfigure 디버깅

--- src/main/java/com/example/elasticsearch/ElasticSearchApplication.java
+++ src/main/java/com/example/elasticsearch/ElasticSearchApplication.java
@@ -1,13 +1,29 @@
 package com.example.elasticsearch;
 
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ElasticSearchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElasticSearchApplication.class, args);
+    }
+
+    //RestHighLevelClient Deprecated
+    @Autowired
+    public RestClient restClient;
+
+    @PostConstruct
+    void postConstruct() {
+        System.out.println(restClient);
     }
 
 }
